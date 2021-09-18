31c5196771e146eb4c64f53c9afb619c65fdbe55
sunitee <suniteebajpai2@gmail.com>
2021-09-18 08:08:42 -0400

    solved rest

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -8,7 +8,10 @@ const API = "http://localhost:3000/sushis"
 class App extends Component {
 
   state= {
-    sushis: []
+    sushis: [],
+    eaten: [],
+    money: 100,
+    displayIndex: 0,
   }
 
   componentDidMount(){
@@ -21,12 +24,42 @@ class App extends Component {
      })
   }
 
+  eat = (sushi) => {
+    const newMoney = this.state.money - sushi.price
+
+    if (!this.state.eaten.includes(sushi) && newMoney >=0 ){
+      this.setState({
+        eaten: [...this.state.eaten, sushi],
+        money: newMoney
+      })
+    }
+  }
+
+  chooseFourSushis= ()=> {
+    return this.state.sushis.slice(this.state.displayIndex, this.state.displayIndex+4)
+  }
+
+  // more = (event) => 
+  //   let newDisplayIndex = this.state.displayIndex + 4
+
+    // if (newDisplayIndex >= this.state.sushis.length){
+    //   newDisplayIndex = 0
+    // }
+    // this.setState({
+    //   displayIndex: newDisplayIndex
+    // })
+  // }
+
   render() {
-    console.log(this.state)
+   
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer  sushis={this.chooseFourSushis()}
+          more={this.more}
+          eat={this.eat}
+          eaten={this.state.eaten}/>
+        <Table  remainingBudget ={this.state.money}
+          eaten={this.state.eaten}/>
       </div>
     );
   }
